9e11b75608d74f003ec6e5e223713fd220b0ac57
mdtaju0908 <er.mdtaju@gmail.com>
2026-04-16 16:32:30 +0530

    chore: update vite version to ^8.0.8 in package.json

--- backend/ats/predictor/ml_loader.py
+++
@@ -1,7 +0,0 @@
-import joblib
-import os
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-MODEL = joblib.load(os.path.join(BASE_DIR, "ml/ats_model.pkl"))
-VECTORIZER = joblib.load(os.path.join(BASE_DIR, "ml/tfidf.pkl"))
--- backend/ats/predictor/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import PredictATS
-
-urlpatterns = [
-    path('ats/score', PredictATS.as_view(), name='ats-score'),
-]
